610236dc29a2308a18324a6a270c2464d9fb6727
yamawade <mariamawade69@gmail.com>
2024-01-26 11:57:23 +0000

    test register commercant

--- tests/Feature/AuthentificationTest.php
+++ tests/Feature/AuthentificationTest.php
@@ -10,25 +10,49 @@ class AuthentificationTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_register_client()
-    {
+    // public function test_register_client()
+    // {
+    //     $data = [
+    //         'nom' => 'Wade',
+    //         'prenom' => 'Mariam',
+    //         'email' => 'mane@gmail.com',
+    //         'password' => 'passer1234',
+    //         'adresse' => 'keur Massar',
+    //         'genre' => 'femme',
+    //         'numero_tel' => '778009876',
+    //         'date_naiss' => '2002-05-12'
+    //     ];
+    
+    //     $response = $this->json('POST', '/api/registerClient', $data);
+    
+    //     $response->assertStatus(200)
+    //              ->assertJson([
+    //                  'status' => 200,
+    //                  'message' => 'Utilisateur créer avec succes',
+    //              ]);
+    // }
+
+    public function test_register_commercant(){
         $data = [
-            'nom' => 'Wade',
-            'prenom' => 'Mariam',
-            'email' => 'mane@gmail.com',
+            'nom' => 'test',
+            'prenom' => 'test',
+            'email' => 'commercant@gmail.com',
             'password' => 'passer1234',
+            'ninea' => '123456789',
             'adresse' => 'keur Massar',
-            'genre' => 'femme',
+            'nin' => '14567896789',
             'numero_tel' => '778009876',
-            'date_naiss' => '2002-05-12'
+            'genre' => 'homme',
+            'date_naiss' => '1990-01-31'
         ];
-    
-        $response = $this->json('POST', '/api/registerClient', $data);
-    
+
+        $response = $this->json('POST', '/api/registerCommercant', $data);
+
         $response->assertStatus(200)
-                 ->assertJson([
-                     'status' => 200,
-                     'message' => 'Utilisateur créer avec succes',
-                 ]);
+            ->assertJson([
+                'status' => 200,
+                'message' => 'Utilisateur créer avec succes',
+            ]);
     }
+
 }
