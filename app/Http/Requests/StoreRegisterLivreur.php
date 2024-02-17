e8c2b8e0231667aa0b151d66a7b27767854529f9
yamawade <mariamawade69@gmail.com>
2024-02-17 18:22:20 +0000

    validation

--- app/Http/Requests/StoreRegisterLivreur.php
+++ app/Http/Requests/StoreRegisterLivreur.php
@@ -24,10 +24,11 @@ class StoreRegisterLivreur extends FormRequest
     public function rules(): array
     {
         return [
-            'nom' => 'required|string|max:255',
-            'prenom' => 'required|string|max:255',
+            'nom' => 'required|string|min:2|max:255',
+            'prenom' => 'required|string|min:2|max:255',
             'matricule' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
+            //'email' => 'required|string|email|max:255|unique:users',
+            'email' => ['required', 'string', 'regex:/^[a-zA-Z_][\w\.-]*@[a-zA-Z][a-zA-Z.-]+\.[a-zA-Z]{2,4}$/','unique:users'],
             'genre'=>'required|string|in:homme,femme',
             'numero_tel' => ['required', 'string', 'regex:/^(77|76|75|78|33)[0-9]{7}$/','unique:users'],
             'password' => 'required|string|min:8',
@@ -60,6 +61,11 @@ class StoreRegisterLivreur extends FormRequest
             'numero_tel.required' => 'Un numéro de téléphone doit être fourni',
             'numero_tel.regex' => 'Le numéro de téléphone doit être au format correct',
             'numero_tel.unique' => 'Un numéro de téléphone existe deja',
+            'email.regex' => 'L\'adresse email doit être au format correct',
+            'nom.min' => 'Le nom doit avoir au moins 2 caractères',
+            'nom.max' => 'Le nom doit avoir au plus 255 caractères',
+            'prenom.min' => 'Le prenom doit avoir au moins 2 caractères',
+            'prenom.max' => 'Le prenom doit avoir au plus 255 caractères',
         ];
     }
 }
