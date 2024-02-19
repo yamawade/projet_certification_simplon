ca7f7e79ce504ee60c6d1d0a2de576093846611f
yamawade <mariamawade69@gmail.com>
2024-02-19 11:27:20 +0000

    produitbycategorie

--- routes/api.php
+++ routes/api.php
@@ -88,7 +88,7 @@ Route::middleware(['auth:api','admin'])->group(function(){
     Route::get('Detailscommandes/{commande}',[CommandeController::class,'show']);
     Route::get('voirProduitSignaler/{produitSignaler}',[AvisController::class,'show']);
     Route::get('bloquerProduitSignaler/{produitSignaler}',[AvisController::class,'bloquerProduitSignaler']);
-    Route::get('Nombreproduits/{categorie}',[ProduitController::class,'getNombreProduitsByCategorie']);
+    Route::get('NombreproduitsByCategorie',[ProduitController::class,'getNombreProduitsByCategorie']);
     Route::post('RepondreFeedback/{feedback}',[FeedbackController::class,'repondreFeedback']);
     Route::post('bloquerUtilisateur/{user}',[AuthController::class,'bloquerUtilisateur']);
 });
